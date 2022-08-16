900641d49ae2218aea06f249fa508e7202ca2fec
praj-inai <108678558+praj-inai@users.noreply.github.com>
2022-08-16 14:56:26 +0530

    minor changes

--- headless-Checkout/client/vanilla/pages/getCardInfo.js
+++ headless-Checkout/client/vanilla/pages/getCardInfo.js
@@ -1,3 +1,10 @@
+//local-server url and token
+//replace this url with your local-server url
+const server = "http://localhost:8080";
+//replace token with client id
+const token = "sbx_ci_7kCbmGnJBYmC4TwUz1FA3FsWPnRKQG3gzCX4R87VDTsS";
+const country = "USA";
+
 const createElement = (tag, id) => {
   const elem = document.createElement(tag);
   if (!!id) {
@@ -16,49 +23,34 @@ function debounce(callback, delay) {
 }
 
 const response_display = document.getElementById('response');
-
-
-
 const input_box = document.getElementById('card_number');
 
 input_box.oninput = (e) => {
   if ((e.target.value.length) >= 6) {
     debounce(cardInfo(), 1500);
   }
-}
 
-// input_box.addEventListener(
-//   "keyup",
-//   console.log(input_box.value)
-// );
-
-// input_box.addEventListener(
-//   "keyup",
-//   debounce(cardInfo, 1500)
-// );
-
-
-
-function cardInfo() {
-  const url = "http://localhost:8080/create";
-  const options = {
-    method: "POST",
-  };
-  fetch(url, options)
-    .then((response) => response.json())
-    .then((data) => {
-      let order_id = (data.id);
-      const card_number = document.getElementById("card_number").value;
-      let inaiInstance = window.inai.create({
-        token: "sbx_ci_7kCbmGnJBYmC4TwUz1FA3FsWPnRKQG3gzCX4R87VDTsS",
-        orderId: order_id,
-        countryCode: "USA",
-      });
-      inaiInstance.getCardInfo(card_number)
-        .then((res) => {
-          response_display.textContent = (JSON.stringify(res));
-        }).catch((error) => {
-          response_display.textContent = (JSON.stringify(error));
+  function cardInfo() {
+    const url = `${server}/create`;
+    const options = {
+      method: "POST",
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        let order_id = (data.id);
+        const card_number = document.getElementById("card_number").value;
+        let inaiInstance = window.inai.create({
+          token: token,
+          orderId: order_id,
+          countryCode: country,
         });
-    });
+        inaiInstance.getCardInfo(card_number)
+          .then((res) => {
+            response_display.textContent = (JSON.stringify(res));
+          }).catch((error) => {
+            response_display.textContent = (JSON.stringify(error));
+          });
+      });
+  }
 }
