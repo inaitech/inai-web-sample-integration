c8bcb84c08f83bb18ad827ed81feaa156f47b20a
Sahil Sachdev <sahil.sachdev@inai.io>
2022-08-17 12:20:34 +0530

    initialize inai

--- dropin-checkout/client/angular/src/app/app.component.ts
+++ dropin-checkout/client/angular/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  initializeInai () {
+    window.inai.initialize({
+      token: 'sbx_ci_4iUs94x9BEgdtwd3JVP9gVmSpqFBwo4uAUCJk1BRfcmb',
+      orderId: 'sbx_ord_2DRYKIP9Cq612F0fEn4yGeQhnWx',
+      countryCode: 'IND',
+      redirectUrl: '',
+      locale: ''
+    });
+  }
 }
